ca9a901cf16ccd2a0834b9d808811942ebc3bbbc
bluolightning <124224350+bluolightning@users.noreply.github.com>
2024-11-15 20:23:58 -0500

    Add password validation

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 // Set overlay width
-width = document.querySelector("img").offsetWidth;
-document.querySelector(".overlay").style.width = `${width}px`;
+let width = document.querySelector('img').offsetWidth;
+document.querySelector('.overlay').style.width = `${width}px`;
+
+window.addEventListener('resize', function() {
+    width = document.querySelector('img').offsetWidth;
+    document.querySelector('.overlay').style.width = `${width}px`;
+});
+
+
+// Validate passwords
+let passwd = document.getElementById('passwd');
+let passwdc = document.getElementById('passwdc');
+
+let passwdValue, passwdcValue;
+
+passwd.addEventListener('input', function() {
+    passwdValue = passwd.value;
+    validatePasswd();
+});
+
+passwdc.addEventListener('input', function() {
+    passwdcValue = passwdc.value;
+    validatePasswd();
+});
+
+function validatePasswd() {
+    if (passwdValue === '' || passwdcValue === '') { 
+        passwd.setCustomValidity('Please enter a password');
+        passwdc.setCustomValidity('Please enter a password');
+        console.log('invalid');
+    } else if (passwdValue === passwdcValue) {
+        passwd.setCustomValidity('');
+        passwdc.setCustomValidity('');
+        console.log('valid');
+    } else {
+        passwd.setCustomValidity('Passwords do not match');
+        passwdc.setCustomValidity('Passwords do not match');
+        console.log('invalid');
+    }
+}
